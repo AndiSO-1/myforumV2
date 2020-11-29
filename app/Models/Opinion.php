389ddc6f825c4c6f014ade8af6fb70f351dfbab9
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-11-29 14:25:10 +0100

    Created Eloquent models
    using php artisan make:model

---
+++ app/Models/Opinion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Opinion extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+}
