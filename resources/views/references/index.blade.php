27f1f0853308349d636c2d5379188061d37c09e7
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-11-29 15:03:52 +0100

    Follow link and show details buttons working

--- resources/views/references/index.blade.php
+++ resources/views/references/index.blade.php
@@ -3,15 +3,15 @@
 @section ('content')
     <h1 class="text-center p-5">Réferences</h1>
     @forelse ($references as $reference)
-        <div id="ref<?= $reference->url ?>" data-id="<?= $reference->id ?>" class="row text-center divtitle">
+        <div class="row text-center divtitle" data-id="{{$reference->id}}">
             <div class="col-2 text-right">
-                <div id="divIcons<?= $reference->id ?>" class="d-none">
-                    <a class="blacklink m-2" href="<?= $reference->url ?>"><i class="fas fa-eye"></i></a>
-                    <a class="blacklink m-2" href="?controller=reference&action=show&id=<?= $reference->id ?>"><i class="fas fa-search-plus"></i></a>
+                <div id="divIcons{{$reference->id}}" class="d-none">
+                    <a class="blacklink m-2" href="{{ $reference->url }}"><i class="fas fa-eye"></i></a>
+                    <a class="blacklink m-2" href="{{ route('references.show',$reference->id) }}"><i class="fas fa-search-plus"></i></a>
                 </div>
             </div>
             <div class="col-10 text-left">
-                <?= $reference->description ?>
+                {{ $reference->description }}
             </div>
         </div>
     @empty
