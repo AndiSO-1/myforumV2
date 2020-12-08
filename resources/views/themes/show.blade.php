3c450a3f42c21a71653f844f426eb67b4af975b9
Andi <andi.santos-oliveira@cpnv.ch>
2020-12-08 10:53:09 +0100

    Display theme

--- resources/views/themes/show.blade.php
+++ resources/views/themes/show.blade.php
@@ -1,5 +1,29 @@
 @extends ('layout')
 
 @section ('content')
-    <h1 class="text-center p-5">Show Theme <?= $id ?></h1>
+    <div class="row">
+        <div class="bg-light col-2 border border-dark">Nom:</div>
+        <div class="col-10">{{ $theme->name }}</div>
+    </div>
+    <div class="row m-3">
+        <a href="#" data-toggle="modal" data-target="#confirm-delete" class="btn btn-danger">Supprimer</a>
+        <a href="{{ route('themes.edit',$theme->id) }}" class="btn btn-primary">Modifier</a>
+    </div>
+    <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body text-center">
+                    Sûr et certain ???
+                </div>
+                <div class="modal-footer flex-row justify-content-center">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                    <form action="{{ route('themes.destroy',$theme->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger btn-ok">Confirmer</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
