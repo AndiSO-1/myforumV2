43c09375a9d12d9b9b5b179630e9220e32b39c43
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2021-01-28 10:03:40 +0100

    Add new data for roles

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -14,23 +14,31 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('roles')->delete();
-        
+
         \DB::table('roles')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
-                'name' => 'Prof',
+                'slug' => 'PROF',
+                'name' => 'Enseignant',
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 2,
-                'name' => 'Stud',
+                'slug' => 'STUD',
+                'name' => 'Etudiant',
+            ),
+            2 =>
+            array (
+                'id' => 3,
+                'slug' => 'ADMI',
+                'name' => 'Administrateur',
             ),
         ));
-        
-        
+
+
     }
 }
